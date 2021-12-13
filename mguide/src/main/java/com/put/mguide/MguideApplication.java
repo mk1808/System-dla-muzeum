47ceb85b741b2927e75e35bb65c0eba31fcf16fb
mk1808 <monikor97@gmail.com>
2021-12-13 16:58:48 +0100

    #1 spring init

---
+++ mguide/src/main/java/com/put/mguide/MguideApplication.java
@@ -0,0 +1,13 @@
+package com.put.mguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MguideApplication.class, args);
+	}
+
+}
---
+++ mguide/src/test/java/com/put/mguide/MguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.put.mguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
