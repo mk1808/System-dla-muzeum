bc26e24fd7462096c884d052b55178e5c31ec04a
mk1808 <monikor97@gmail.com>
2021-12-15 01:22:15 +0100

    #4 button and text, layout

--- mguide/src/main/mobile/mguide-admin/mguide/lib/mainPage.dart
+++ mguide/src/main/mobile/mguide-admin/mguide/lib/mainPage.dart
@@ -10,8 +10,12 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   final _biggerFont = const TextStyle(fontSize: 100.0);
-  getContainer(){
-     return Container(
+  final _biggerFont2 =
+      const TextStyle(fontSize: 25.0, fontWeight: FontWeight.w600);
+      double iconSize = 40;
+  final midFont = const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w300);
+  getContainer() {
+    return Container(
       color: Colors.green,
       child: Material(
         child: ListTile(
@@ -21,16 +25,140 @@ class _MainPageState extends State<MainPage> {
       ),
     );
   }
+
+  getCenter() {
+    return const Center(
+      child: Material(
+        child: ListTile(
+          title: Text('glowna'),
+          tileColor: Colors.red,
+        ),
+      ),
+    );
+  }
+
+  getCenter2() {
+    return Center(
+      //child: Material(
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [Text('170 Reviews'), Text('170 Reviews'), getButton()]),
+      //),
+    );
+  }
+
+  getCenter3() {
+    return Center(
+        child: Column(
+      //  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 10.0),
+          height: 200.0,
+          // color: Colors.yellow,
+          child: Text('Aplikacja administratora systemu MGuide dla Muzeum ABC',
+              textAlign: TextAlign.center, style: _biggerFont2),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+          height: 300.0,
+          // color: Colors.yellow,
+          child: Text(
+              'Za pomocą tego narzędzia można powiązać eksponaty z punktami w pomieszczeniach muzeum.W tym celu należy wejść w tryb wiązania, następnie przejść w miejsce, gdzie znajduje się eksponat. Będąc na miejscu należy nacisnąć przycisk "Powiąż miejsce z eksponatem", a następnie wybrać z listy dostępnych pozycji odpowiedni eksponat.',
+              textAlign: TextAlign.center,
+              style: midFont),
+        ), Container(
+          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+         
+         
+          child:SizedBox(
+  width: 300, 
+  height: 50,
+  child: getButton()
+)
+    
+        ), Container(
+          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+         
+         
+  
+          child:getTab()
+        )
+      ],
+    ));
+  }
+
+  getRow() {
+    return Row(children: [
+      const Text('170 Reviews'),
+      const Text('170 Reviews'),
+      getButton()
+    ]);
+  }
+
+  getButton() {
+    return  ElevatedButton.icon(
+          icon: Icon(
+            Icons.favorite,
+            color: Colors.pink,
+            size: 24.0,
+          ),
+          label: Text('Elevated Button'),
+          onPressed: () {
+            print('Pressed');
+          },style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+      
+        ));}
+    
+    
+    
+  /*  ElevatedButton(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+      
+        ),
+        onPressed: () {},
+        child: Text('TextButton'),
+        
+        );*/
+ 
+getTab(){
+ return Container(
+              margin: EdgeInsets.all(10),
+              child: Table(
+                border: TableBorder.all(),
+                children: [
+                  TableRow( children: [
+                    Column(children:[
+                      Icon(Icons.account_box, size: iconSize,),
+                      Text('My Account')
+                    ]),
+                    Column(children:[
+                      Icon(Icons.settings, size: iconSize,),
+                      Text('Settings')
+                    ]),
+                    Column(children:[
+                      Icon(Icons.lightbulb_outline, size: iconSize,),
+                      Text('Ideas')
+                    ]),
+                  ]),
+                  TableRow( children: [
+                    Icon(Icons.cake, size: iconSize,),
+                    Icon(Icons.voice_chat, size: iconSize,),
+                    Icon(Icons.add_location, size: iconSize,),
+                  ]),
+                ],
+              ),
+            );
+}
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Strona główna'),
-      ),
-      drawer:menu(),
-      body: getContainer(),
-    );
-   
+        appBar: AppBar(
+          title: const Text('Strona główna'),
+        ),
+        drawer: menu(),
+        body: getCenter3());
   }
 }
