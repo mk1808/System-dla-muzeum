7a64fdd5b3971db165147cc9536dc89716a3ccc5
mk1808 <monikor97@gmail.com>
2021-12-15 01:22:31 +0100

    #4 format

--- mguide/src/main/mobile/mguide-admin/mguide/lib/mainPage.dart
+++ mguide/src/main/mobile/mguide-admin/mguide/lib/mainPage.dart
@@ -12,7 +12,7 @@ class _MainPageState extends State<MainPage> {
   final _biggerFont = const TextStyle(fontSize: 100.0);
   final _biggerFont2 =
       const TextStyle(fontSize: 25.0, fontWeight: FontWeight.w600);
-      double iconSize = 40;
+  double iconSize = 40;
   final midFont = const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w300);
   getContainer() {
     return Container(
@@ -67,23 +67,15 @@ class _MainPageState extends State<MainPage> {
               'Za pomocą tego narzędzia można powiązać eksponaty z punktami w pomieszczeniach muzeum.W tym celu należy wejść w tryb wiązania, następnie przejść w miejsce, gdzie znajduje się eksponat. Będąc na miejscu należy nacisnąć przycisk "Powiąż miejsce z eksponatem", a następnie wybrać z listy dostępnych pozycji odpowiedni eksponat.',
               textAlign: TextAlign.center,
               style: midFont),
-        ), Container(
-          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
-         
-         
-          child:SizedBox(
-  width: 300, 
-  height: 50,
-  child: getButton()
-)
-    
-        ), Container(
-          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
-         
-         
-  
-          child:getTab()
-        )
+        ),
+        Container(
+            padding:
+                const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+            child: SizedBox(width: 300, height: 50, child: getButton())),
+        Container(
+            padding:
+                const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+            child: getTab())
       ],
     ));
   }
@@ -97,22 +89,21 @@ class _MainPageState extends State<MainPage> {
   }
 
   getButton() {
-    return  ElevatedButton.icon(
-          icon: Icon(
-            Icons.favorite,
-            color: Colors.pink,
-            size: 24.0,
-          ),
-          label: Text('Elevated Button'),
-          onPressed: () {
-            print('Pressed');
-          },style: ButtonStyle(
+    return ElevatedButton.icon(
+        icon: Icon(
+          Icons.favorite,
+          color: Colors.pink,
+          size: 24.0,
+        ),
+        label: Text('Elevated Button'),
+        onPressed: () {
+          print('Pressed');
+        },
+        style: ButtonStyle(
           foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-      
-        ));}
-    
-    
-    
+        ));
+  }
+
   /*  ElevatedButton(
         style: ButtonStyle(
           foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
@@ -122,36 +113,55 @@ class _MainPageState extends State<MainPage> {
         child: Text('TextButton'),
         
         );*/
- 
-getTab(){
- return Container(
-              margin: EdgeInsets.all(10),
-              child: Table(
-                border: TableBorder.all(),
-                children: [
-                  TableRow( children: [
-                    Column(children:[
-                      Icon(Icons.account_box, size: iconSize,),
-                      Text('My Account')
-                    ]),
-                    Column(children:[
-                      Icon(Icons.settings, size: iconSize,),
-                      Text('Settings')
-                    ]),
-                    Column(children:[
-                      Icon(Icons.lightbulb_outline, size: iconSize,),
-                      Text('Ideas')
-                    ]),
-                  ]),
-                  TableRow( children: [
-                    Icon(Icons.cake, size: iconSize,),
-                    Icon(Icons.voice_chat, size: iconSize,),
-                    Icon(Icons.add_location, size: iconSize,),
-                  ]),
-                ],
+
+  getTab() {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: Table(
+        border: TableBorder.all(),
+        children: [
+          TableRow(children: [
+            Column(children: [
+              Icon(
+                Icons.account_box,
+                size: iconSize,
               ),
-            );
-}
+              Text('My Account')
+            ]),
+            Column(children: [
+              Icon(
+                Icons.settings,
+                size: iconSize,
+              ),
+              Text('Settings')
+            ]),
+            Column(children: [
+              Icon(
+                Icons.lightbulb_outline,
+                size: iconSize,
+              ),
+              Text('Ideas')
+            ]),
+          ]),
+          TableRow(children: [
+            Icon(
+              Icons.cake,
+              size: iconSize,
+            ),
+            Icon(
+              Icons.voice_chat,
+              size: iconSize,
+            ),
+            Icon(
+              Icons.add_location,
+              size: iconSize,
+            ),
+          ]),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
