6891d4cb0c31e965c73453615a2648b5deecb214
mk1808 <monikor97@gmail.com>
2021-12-16 21:12:56 +0100

    #4 table view and dialog

--- mguide/src/main/mobile/mguide-admin/mguide/lib/chooseExhibitForMapping.dart
+++ mguide/src/main/mobile/mguide-admin/mguide/lib/chooseExhibitForMapping.dart
@@ -1,24 +1,252 @@
 import 'package:flutter/material.dart';
 import 'package:mguide/menu.dart';
+import 'package:mguide/successMapping.dart';
+
 class ChooseExhibitForMapping extends StatefulWidget {
-  const ChooseExhibitForMapping({ Key? key }) : super(key: key);
+  const ChooseExhibitForMapping({Key? key}) : super(key: key);
 
   @override
-  _ChooseExhibitForMappingState createState() => _ChooseExhibitForMappingState();
+  _ChooseExhibitForMappingState createState() =>
+      _ChooseExhibitForMappingState();
 }
 
 class _ChooseExhibitForMappingState extends State<ChooseExhibitForMapping> {
+  final _biggerFont2 =
+      const TextStyle(fontSize: 27.0, fontWeight: FontWeight.w600);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Rozpocznij mapowanie'),
+          title: const Text('Mapowanie ekspozycji'),
         ),
         drawer: menu(),
-        body: getCenter3());
+        body: // MaterialApp(home:
+            wholeTable() //)
+        );
   }
 
-    getCenter3() {
-  
+  wholeTable() {
+    return ListView(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        children: [
+          Container(
+              // height: 400,
+              padding: EdgeInsets.all(4),
+              margin: EdgeInsets.only(left: 8, top: 8, right: 8),
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(12.0, 40.0, 12.0, 10.0),
+                    height: 160.0,
+                    child: Text('Wybierz obiekt z listy',
+                        textAlign: TextAlign.center, style: _biggerFont2),
+                  ),
+                  Container(
+                      padding:
+                          const EdgeInsets.fromLTRB(12.0, 10.0, 12.0, 20.0),
+                      //height: 1500.0,
+                      child: getInput()),
+                       Container(
+                      padding:
+                          const EdgeInsets.fromLTRB(12.0, 40.0, 12.0, 20.0),
+                      height: 1500.0,
+                      child: getTableWithScroll()),
+                ],
+              )),
+        ]);
+  }
+
+  getTableWithScroll() {
+    return ListView(scrollDirection: Axis.horizontal,
+        // shrinkWrap: true,
+
+        //   height: 400,
+        children: [
+          Container(
+              color: Colors.white,
+              margin: EdgeInsets.only(left: 8, right: 8),
+              //height: 500,
+              child: getTable())
+        ]);
+  }
+
+  getInput() {
+    return TextFormField(
+      decoration: const InputDecoration(
+        hintText: 'Wpisz nazwę',
+      ),
+      validator: (String? value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter some text';
+        }
+        return null;
+      },
+    );
+  }
+onTapOk(){
+   Navigator.push(context,
+              MaterialPageRoute(builder: (context) => SuccessMapping()));
+}
+  getTable() {
+    return DataTable(
+      showCheckboxColumn: false,
+      columns: [
+        DataColumn(
+            label: Text('ID',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+        DataColumn(
+            label: Text('Name',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+        DataColumn(
+            label: Text('Profession',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+        DataColumn(
+            label: Text('Profession1',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+      ],
+      rows: [
+        DataRow(cells: [
+          DataCell(Text('1')),
+          DataCell(Text('Stephen')),
+          DataCell(Text('Actor')),
+          DataCell(Text('Actor')),
+        ],
+        onSelectChanged: (newValue) {
+                showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('Mapowanie'),
+          content: const Text('Czy powiązać obecne miejsce z obiektem?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Wróć'),
+            ),
+            TextButton(
+              onPressed: () => {onTapOk()},
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+            },
+        ),
+        DataRow(cells: [
+          DataCell(Text('5')),
+          DataCell(Text('John')),
+          DataCell(Text('Student')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('10')),
+          DataCell(Text('Harry')),
+          DataCell(Text('Leader')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('15')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('15')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('15')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('15')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('15')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('15')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('15')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('15')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('15')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('15')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('15')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('15')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('15')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('15')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('15')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('15')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+        DataRow(cells: [
+          DataCell(Text('16')),
+          DataCell(Text('Peter')),
+          DataCell(Text('Scientist')),
+          DataCell(Text('Actor')),
+        ]),
+      ],
+    );
   }
 }
