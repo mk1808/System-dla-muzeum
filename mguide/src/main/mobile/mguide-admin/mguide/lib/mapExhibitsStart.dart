9bb88cb5305e8ab55460530bace1f91837fe780f
mk1808 <monikor97@gmail.com>
2021-12-16 15:46:59 +0100

    #4 adding photo

--- mguide/src/main/mobile/mguide-admin/mguide/lib/mapExhibitsStart.dart
+++ mguide/src/main/mobile/mguide-admin/mguide/lib/mapExhibitsStart.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mguide/menu.dart';
 
 class MapExhibitsStart extends StatefulWidget {
-  const MapExhibitsStart({ Key? key }) : super(key: key);
+  const MapExhibitsStart({Key? key}) : super(key: key);
 
   @override
   _MapExhibitsStartState createState() => _MapExhibitsStartState();
@@ -10,17 +10,246 @@ class MapExhibitsStart extends StatefulWidget {
 
 class _MapExhibitsStartState extends State<MapExhibitsStart> {
   final _biggerFont = const TextStyle(fontSize: 100.0);
-  getContainer(){
-     return Container(
+  final _biggerFont2 =
+      const TextStyle(fontSize: 27.0, fontWeight: FontWeight.w600);
+  double iconSize = 40;
+  final midFont = const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w300);
+  getContainer() {
+    return Container(
       color: Colors.green,
       child: Material(
         child: ListTile(
-          title: Text('Mapowanie eksponatów', style: _biggerFont),
+          title: Text('glowna', style: _biggerFont),
           tileColor: Colors.red,
         ),
       ),
     );
   }
+
+  getCenter() {
+    return const Center(
+      child: Material(
+        child: ListTile(
+          title: Text('glowna'),
+          tileColor: Colors.red,
+        ),
+      ),
+    );
+  }
+
+  getCenter2() {
+    return Center(
+      //child: Material(
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [Text('170 Reviews'), Text('170 Reviews'), getButton()]),
+      //),
+    );
+  }
+
+  getCenter3() {
+    return ListView(scrollDirection: Axis.vertical, shrinkWrap: true, children: [
+      Center(
+          child: Column(
+        //  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(12.0, 40.0, 12.0, 20.0),
+            height: 160.0,
+            // color: Colors.yellow,
+            child: Text(
+                'Aplikacja administratora systemu MGuide dla Muzeum ABC',
+                textAlign: TextAlign.center,
+                style: _biggerFont2),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
+            height: 300.0,
+            // color: Colors.yellow,
+            child: Text(
+                'Za pomocą tego narzędzia można powiązać eksponaty z punktami w pomieszczeniach muzeum. W tym celu należy wejść w tryb wiązania, następnie przejść w miejsce, gdzie znajduje się eksponat. Będąc na miejscu należy nacisnąć przycisk "Powiąż miejsce z eksponatem", a następnie wybrać z listy dostępnych pozycji odpowiedni eksponat.',
+                textAlign: TextAlign.center,
+                style: midFont),
+          ),
+          Container(
+              padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
+              height: 300.0,
+              // color: Colors.yellow,
+              child: Image(image: AssetImage('assets/index.jpg'))),
+          Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+              child: SizedBox(width: 300, height: 50, child: getButton())),
+          Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+              child: getTab()),
+          Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+              child: getInput())
+        ],
+      ))
+    ]);
+  }
+getInput(){
+return TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Enter your email',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          );
+}
+  getRow() {
+    return Row(children: [
+      const Text('170 Reviews'),
+      const Text('170 Reviews'),
+      getButton()
+    ]);
+  }
+
+  getButton() {
+    return ElevatedButton.icon(
+        label: Text('Rozpocznij mapowanie'),
+        icon: Icon(
+          Icons.east,
+          color: Colors.white,
+          size: 30.0,
+        ),
+        onPressed: () {
+          print('Pressed');
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => MapExhibitsStart()));
+        },
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+        ));
+  }
+
+  /*  ElevatedButton(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+      
+        ),
+        onPressed: () {},
+        child: Text('TextButton'),
+        
+        );*/
+  getTab2() {
+    return ListView(children: <Widget>[
+      Center(
+          heightFactor: 20.0,
+          child: Text(
+            'People-Chart',
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          )),
+      DataTable(
+        columns: [
+          DataColumn(
+              label: Text('ID',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+          DataColumn(
+              label: Text('Name',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+          DataColumn(
+              label: Text('Profession',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+        ],
+        rows: [
+          DataRow(cells: [
+            DataCell(Text('1')),
+            DataCell(Text('Stephen')),
+            DataCell(Text('Actor')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('5')),
+            DataCell(Text('John')),
+            DataCell(Text('Student')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('10')),
+            DataCell(Text('Harry')),
+            DataCell(Text('Leader')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('15')),
+            DataCell(Text('Peter')),
+            DataCell(Text('Scientist')),
+          ]),
+        ],
+      ),
+    ]);
+  }
+
+  getTab() {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: Table(
+        border: TableBorder.all(),
+        children: [
+          TableRow(children: [
+            Column(children: [
+              Icon(
+                Icons.account_box,
+                size: iconSize,
+              ),
+              Text('My Account')
+            ]),
+            Column(children: [
+              Icon(
+                Icons.settings,
+                size: iconSize,
+              ),
+              Text('Settings')
+            ]),
+            Column(children: [
+              Icon(
+                Icons.lightbulb_outline,
+                size: iconSize,
+              ),
+              Text('Ideas')
+            ]),
+          ]),
+          TableRow(children: [
+            Icon(
+              Icons.cake,
+              size: iconSize,
+            ),
+            Icon(
+              Icons.voice_chat,
+              size: iconSize,
+            ),
+            Icon(
+              Icons.add_location,
+              size: iconSize,
+            ),
+          ]),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Rozpocznij mapowanie'),
+        ),
+        drawer: menu(),
+        body: getCenter3());
+  }
+}
+
+/*Icon(
+          Icons.favorite,
+          color: Colors.pink,
+          size: 24.0,
+        ), 
 
   @override
   Widget build(BuildContext context) {
@@ -32,4 +261,4 @@ class _MapExhibitsStartState extends State<MapExhibitsStart> {
       body: getContainer()
     );
   }
-}
+}*/
