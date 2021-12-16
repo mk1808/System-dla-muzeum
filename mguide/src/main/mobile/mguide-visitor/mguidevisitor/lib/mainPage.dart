3e583a60ae0b84e0b0258fba28e90cd7367690cc
mk1808 <monikor97@gmail.com>
2021-12-17 00:00:30 +0100

    #5 config, theme, routing, basic view

---
+++ mguide/src/main/mobile/mguide-visitor/mguidevisitor/lib/mainPage.dart
@@ -0,0 +1,207 @@
+
+import 'package:flutter/material.dart';
+import 'package:mguidevisitor/menu.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({ Key? key }) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+   final _biggerFont = const TextStyle(fontSize: 100.0);
+  final _biggerFont2 =
+      const TextStyle(fontSize: 27.0, fontWeight: FontWeight.w600);
+  double iconSize = 40;
+  final midFont = const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w300);
+  getContainer() {
+    return Container(
+      color: Colors.green,
+      child: Material(
+        child: ListTile(
+          title: Text('glowna', style: _biggerFont),
+          tileColor: Colors.red,
+        ),
+      ),
+    );
+  }
+
+  getCenter() {
+    return const Center(
+      child: Material(
+        child: ListTile(
+          title: Text('glowna'),
+          tileColor: Colors.red,
+        ),
+      ),
+    );
+  }
+
+  getCenter2() {
+    return Center(
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [Text('170 Reviews'), Text('170 Reviews'), getButton()]),
+      
+    );
+  }
+
+  getCenter3() {
+    return Center(
+        child: Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.fromLTRB(12.0, 40.0, 12.0, 20.0),
+          height: 160.0,
+          child: Text('Aplikacja administratora systemu MGuide dla Muzeum ABC',
+              textAlign: TextAlign.center, style: _biggerFont2),
+        ),
+        Container(
+          padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
+          height: 300.0,
+          child: Text(
+              'Za pomocą tego narzędzia można powiązać eksponaty z punktami w pomieszczeniach muzeum. W tym celu należy wejść w tryb wiązania, następnie przejść w miejsce, gdzie znajduje się eksponat. Będąc na miejscu należy nacisnąć przycisk "Powiąż miejsce z eksponatem", a następnie wybrać z listy dostępnych pozycji odpowiedni eksponat.',
+              textAlign: TextAlign.center,
+              style: midFont),
+        ),
+        Container(
+            padding:
+                const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+            child: SizedBox(width: 300, height: 50, child: getButton())),
+      ],
+    ));
+  }
+
+  getRow() {
+    return Row(children: [
+      const Text('170 Reviews'),
+      const Text('170 Reviews'),
+      getButton()
+    ]);
+  }
+
+  getButton() {
+    return ElevatedButton.icon(
+        label: Text('Rozpocznij mapowanie'),
+        icon: Icon(
+          Icons.east,
+          color: Colors.white,
+          size: 30.0,
+        ),
+        onPressed: () {
+          print('Pressed');
+         // Navigator.push(context,
+              //MaterialPageRoute(builder: (context) => StartExhibitsMapping()));
+        },
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+        ));
+  }
+
+getTab2() {
+    return ListView(children: <Widget>[
+      Center(
+        heightFactor: 20.0,
+          child: Text(
+        'People-Chart',
+        style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+      )),
+      DataTable(
+        columns: [
+          DataColumn(
+              label: Text('ID',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+          DataColumn(
+              label: Text('Name',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+          DataColumn(
+              label: Text('Profession',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+        ],
+        rows: [
+          DataRow(cells: [
+            DataCell(Text('1')),
+            DataCell(Text('Stephen')),
+            DataCell(Text('Actor')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('5')),
+            DataCell(Text('John')),
+            DataCell(Text('Student')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('10')),
+            DataCell(Text('Harry')),
+            DataCell(Text('Leader')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('15')),
+            DataCell(Text('Peter')),
+            DataCell(Text('Scientist')),
+          ]),
+        ],
+      ),
+    ]);
+  } 
+
+
+  getTab() {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: Table(
+        border: TableBorder.all(),
+        children: [
+          TableRow(children: [
+            Column(children: [
+              Icon(
+                Icons.account_box,
+                size: iconSize,
+              ),
+              Text('My Account')
+            ]),
+            Column(children: [
+              Icon(
+                Icons.settings,
+                size: iconSize,
+              ),
+              Text('Settings')
+            ]),
+            Column(children: [
+              Icon(
+                Icons.lightbulb_outline,
+                size: iconSize,
+              ),
+              Text('Ideas')
+            ]),
+          ]),
+          TableRow(children: [
+            Icon(
+              Icons.cake,
+              size: iconSize,
+            ),
+            Icon(
+              Icons.voice_chat,
+              size: iconSize,
+            ),
+            Icon(
+              Icons.add_location,
+              size: iconSize,
+            ),
+          ]),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Strona główna'),
+        ),
+        drawer: Menu(),
+        body: getCenter3());
+  }
+}
+
---
+++ mguide/src/main/mobile/mguide-visitor/mguidevisitor/lib/menu.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:mguidevisitor/mainPage.dart';
+
+class Menu extends StatefulWidget {
+  const Menu({ Key? key }) : super(key: key);
+
+  @override
+  _MenuState createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  navigateTo(context, widget) {
+    Navigator.pop(context);
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute<void>(
+        builder: (context) {
+          return widget;
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.cyan[600],///tło nagłówka drawera
+            ),
+            child: Text(
+              'Aplikacja Mguide',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          ListTile(
+              leading: Icon(Icons.star_rounded ),
+              title: Text('Strona główna'),
+              onTap: () => navigateTo(context, new MainPage())),
+         /* ListTile(
+              leading: Icon(Icons.star_rounded ),
+              title: Text('Mapowanie eksponatów'),
+              onTap: () => navigateTo(context, new StartExhibitsMapping())),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () => navigateTo(context, new RandomWords())),
+            ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('TEST'),
+            onTap: () => navigateTo(context, new MyTable())),
+            ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('ChooseExhibitForMapping'),
+            onTap: () => navigateTo(context, new ChooseExhibitForMapping())),
+ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('SuccessMapping'),
+            onTap: () => navigateTo(context, new SuccessMapping())),
+           */ 
+        ],
+      ),
+    );
+  }
+}
