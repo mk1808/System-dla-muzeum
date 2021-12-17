1bca656865c40b68a53f6b30df9853b08c4892c4
mk1808 <monikor97@gmail.com>
2021-12-17 22:50:28 +0100

    #3 fix

--- mguide/src/main/ts/mguide/src/app/auth/auth.component.ts
+++ mguide/src/main/ts/mguide/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
   }
+  onLoginCLick(){
+    this.router.navigate(['/filters']);
+  }
 
 }
+
