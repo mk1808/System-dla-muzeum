f8c56f18d080fce59b5ca0657c0949e8d33460be
mk1808 <monikor97@gmail.com>
2022-01-17 23:25:00 +0100

    #3 security config

--- mguide/src/main/java/com/put/mguide/MguideApplication.java
+++ mguide/src/main/java/com/put/mguide/MguideApplication.java
@@ -2,8 +2,9 @@ package com.put.mguide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication( exclude = {SecurityAutoConfiguration.class} )
 public class MguideApplication {
 
 	public static void main(String[] args) {
